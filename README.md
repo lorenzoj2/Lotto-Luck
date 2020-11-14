ce033aec7acb32c3893da32cd67b5d829cd5e58e
John <suttontaj13@gmail.com>
2020-11-13 22:05:32 -0500

    Added function that returns a ticket's data Added function that combines every ticket into one Dataframe Added overview to README.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Lotto-Luck
+
+## Overview
+This is a program that scrapes scratch off ticket data from the Ohio Lottery website. 
+It generates a CSV file that includes every ticket's name, id, price, odds of winning,
+prizes remaining, picture link, and the time collected.
--- lotto_scrape.py
+++ lotto_scrape.py
@@ -1,9 +1,14 @@
+from datetime import datetime
 from bs4 import BeautifulSoup
 import urllib.request
+import pandas as pd
 
 
 def get_ticket_urls():
-    """Returns the url for every available scratch off ticket """
+    """
+    Returns a list of URLs for every available scratch off ticket
+    """
+
     # base url to all scratch off games
     url = "https://www.ohiolottery.com/Games/ScratchOffs"
 
@@ -13,17 +18,82 @@ def get_ticket_urls():
     # html format that is able to be parsed
     page = BeautifulSoup(content, 'html.parser')
 
+    # list of every ticket's url
     ticket_urls = []
 
     for ticket in page.find_all(class_='igLandListItem'):
-        ticket_urls.append(ticket.find('a')['href'])
+        ticket_urls.append("https://www.ohiolottery.com/" + ticket.find('a')['href'])
 
     return ticket_urls
 
 
+def get_ticket(url):
+    """
+    Returns the available data for a scratch off given the URL
+    """
+
+    # download url as html
+    content = urllib.request.urlopen(url).read()
+
+    # html format that is able to be parsed
+    page = BeautifulSoup(content, 'html.parser')
+
+    # time data was scraped
+    now = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+
+    # collect information about the ticket
+    ticket_name = page.find("h1").text.strip()
+
+    ticket_id = page.find('span', {'class': 'number'}).text.strip("#")
+
+    ticket_price = url.split("/")[6].strip("$")
+    if ticket_price == "20DollarGames":
+        ticket_price = "20-Games"
+    elif ticket_price == "10DollarGames":
+        ticket_price = "10-Games"
+    ticket_price = ticket_price.split('-')[0]
+
+    try:
+        ticket_odds = page.find(class_="odds").text.strip("Overall odds of winning: ").split()[2]
+        ticket_odds = pd.to_numeric(ticket_odds)
+    except IndexError:
+        ticket_odds = 0.0   # no info available
+
+    ticket_tier = []    # ticket prize amounts
+    ticket_rem = []     # ticket prizes remaining
+
+    for game in page.find_all(True, {'class': ['tpdPrizeCell', 'tpdRemainCell']}):
+        if game['class'] == ['tpdPrizeCell']:
+            ticket_tier.append(game.text.strip())
+        else:
+            ticket_rem.append(game.text.strip())
+
+    # prize table for the ticket
+    ticket_prize = dict(zip(ticket_tier, ticket_rem))
+
+    # url to ticket's image
+    ticket_pic = page.find(class_="igTicketImg")["style"]
+    ticket_pic = "https://www.ohiolottery.com" + ticket_pic[ticket_pic.find("(") + 1:ticket_pic.find(")")]
+
+    return [ticket_name, ticket_id, ticket_price, ticket_odds, ticket_prize, ticket_pic, now]
+
+
+def get_tickets_df():
+    """
+    Returns a Dataframe of every ticket's information
+    """
+
+    data = []
+
+    for url in get_ticket_urls():
+        data.append(get_ticket(url))
+
+    return pd.DataFrame(data, columns=['Name', 'ID', 'Price', 'Odds', 'Prize', 'Pic', 'Time'])
+
+
 def main():
-    for ticket in get_ticket_urls():
-        print(ticket)
+    df = get_tickets_df()
+    df.to_csv("lottery_data.csv", index=False)
 
 
 if __name__ == '__main__':
